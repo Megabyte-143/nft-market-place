538dc05bc1cc1d54354a9a7c9aa8399a4da81c8d
Yash Garg <yashygkf@gmail.com>
2022-01-17 10:31:58 +0530

    private key hidden

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 const fs = require("fs");
 
 const projectID = "2c343c676d29bc70644029b116a36e71a0206ee0";
-const pubKey = '0x269B19b8C8A64B2d98A9E833F1B4DaF858862436';
-// const prvKey = "383d2fe68c5e490a1f55156a3f36e535e177eba6a02533d0c8d0b1c654f5df12";
 const prvKey = fs.readFileSync(".secret").toString();
 
 module.exports = {
