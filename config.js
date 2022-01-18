bd146ff12e93ccfc538435e20fe81c230a684e80
Yash Garg <yashygkf@gmail.com>
2022-01-18 19:17:36 +0530

    rpc url updated

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const nftAddress = "0x49cdc6944b9eae453818ca0ccab4e73a41bb21a4";
 export const nftMarketAddress = "0x952996ae2d8b20bbb221f3bedba800529bbb2650";
-export const rpc_url = "https://rpc-mumbai.maticvigil.com";
+export const rpc_url = "https://rpc-mumbai.maticvigil.com/v1/2c343c676d29bc70644029b116a36e71a0206ee0";
